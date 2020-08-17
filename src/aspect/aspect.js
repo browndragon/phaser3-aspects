d11458163bb9b2c6d3a53aa5d54ef5c9d8f3d9a0
Brown Dragon <browndragon@googlegroups.com>
2020-08-17 11:03:33 -0400

    checkpoint refactor

--- src/aspect/aspect.js
+++ src/aspect/aspect.js
@@ -37,44 +37,6 @@ export default class Aspect extends BaseAspect {
         throw 'unimplemented';
     }
 }
-Aspect.Group = class extends Group {
-    constructor(aspectClass, ...params) {
-        super(...params);
-        this.aspect = aspectClass;
-    }
-    addAspect(child) {
-        console.assert(!child[this.aspect.key]);
-        let aspect = new this.aspect(child);
-        if (aspect) {
-            child[this.aspect.key] = aspect;
-        }
-        return aspect;
-    }
-    removeAspect(child) {
-        let aspect = child && child[this.aspect.key];
-        if (!aspect) {
-            return;
-        }
-        aspect.destructor();
-    }
-    init(data) {
-        this.aspect.init(this, data);
-    }
-    preload() {
-        this.aspect.preload(this);
-    }
-    create(data) {
-        this.aspect.create(this, data);
-    }
-    update(time, delta) {
-        let result = this.aspect.update(this, time, delta);
-        if (!result) {
-            return;
-        }
-        for (let child of this.getChildren()) {
-            child[this.aspect.key].update(result);
-        }
-    }
-}
+Aspect.Group = Group;
 
 const O = Symbol('Object');
--- src/aspect/group.js
+++ src/aspect/group.js
@@ -1,22 +1,61 @@
 import Phaser from 'phaser';
 
 export default class Group extends Phaser.GameObjects.Group {
-    add(child, addToScene) {
-        if (this.addAspect(child)) {
-            super.add(child, addToScene);
-        }
+    constructor(aspectClass, ...params) {
+        super(...params);
+        this.aspect = aspectClass;
     }
+
+    get key() {
+        return this.aspect.key;
+    }
+
+    add(child, addToScene) {
+        if (!this.addAspect(child)) {
+            return this;
+        }
+        return this.rawAdd(child, addToScene);
+    }
+    rawAdd(child, addToScene) {
+        return super.add(child, addToScene);
+    }
+
     remove(child, removeFromScene, destroyChild) {
         this.removeAspect(child);
         return super.remove(child, removeFromScene, destroyChild);
     }
-    addAspect(child) {
-        return undefined;
-    }
-    removeAspect(child) {}
 
-    init(data) {}
-    preload() {}
-    create(data) {}
-    update(time, delta) {}
+    addAspect(child) {
+        console.assert(!child[this.aspect.key]);
+        let aspect = new this.aspect(child);
+        if (aspect) {
+            child[this.aspect.key] = aspect;
+        }
+        return aspect;
+    }
+    removeAspect(child) {
+        let aspect = child && child[this.aspect.key];
+        if (!aspect) {
+            return;
+        }
+        aspect.destructor();
+    }
+    init(data) {
+        this.aspect.init(this, data);
+    }
+    preload() {
+        this.aspect.preload(this);
+    }
+    create(data) {
+        this.aspect.create(this, data);
+    }
+    update(time, delta) {
+        let result = this.aspect.update(this, time, delta);
+        if (!result) {
+            return;
+        }
+        for (let child of this.getChildren()) {
+            child[this.aspect.key].update(result);
+        }
+    }
 }
